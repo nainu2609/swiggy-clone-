8f0f93168328630e7bc195199cb2c7b2bc30a023
Nancy Gahlot <nancygahlot1435@gmail.com>
2023-12-25 17:23:53 +0530

    add shimmer effects

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -19,6 +19,7 @@ const Shimmer = () => {
       <div className="shimmer-card"></div>
       <div className="shimmer-card"></div>
       <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
     </div>
   );
 };
