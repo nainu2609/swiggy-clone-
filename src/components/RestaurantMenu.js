0d76de0fe8ebfe478b126cd9028ca25057a703b0
Nancy Gahlot <nancygahlot1435@gmail.com>
2023-12-27 20:54:02 +0530

    "adding  coding"

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -27,7 +27,6 @@ const RestaurantMenu = () => {
         c.card?.["card"]?.["@type"] ===
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
     );
-  //console.log(categories);
 
   return (
     <div className="text-center">
